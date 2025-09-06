68e7bb6b44a9efe7d9f84f0260dd2cc9521b22bb
Christopher Andrew Topalian <scriptingdata@gmail.com>
2025-09-06 12:05:28 -0400

    update

--- src/py/filter/filterShowAll.py
+++ src/py/filter/filterShowAll.py
@@ -1,4 +1,5 @@
 # filterShowAll.py
+# returns all the items of a list
 
 people = [
     {
@@ -20,24 +21,34 @@ people = [
 ####
 
 def filterShowAll(whichList):
+    # start with an empty list
     result = []
 
+    # loop through every index in the given list
     for z in range(len(whichList)):
+        # append (add) each item into the result list
         result.append(whichList[z])
 
+    # return the new list containing all items
     return result
 
 ####
 
 if __name__ == '__main__':
+    # call the function with our people list
     allPeople = filterShowAll(people)
 
+    # print the entire list so we can see the result
     print(allPeople)
+
+    input('Press Enter to Exit')
 
 ####
 
 '''
-[{'name': 'Jane', 'date': '2024-05-01 10:00:00'}, {'name': 'Jennifer', 'date': '2023-03-12 15:30:00'}, {'name': 'Tabitha', 'date': '2024-05-09 08:45:00'}]
+[{'name': 'Jane', 'date': '2024-05-01 10:00:00'},
+ {'name': 'Jennifer', 'date': '2023-03-12 15:30:00'},
+ {'name': 'Tabitha', 'date': '2024-05-09 08:45:00'}]
 '''
 
 ####
