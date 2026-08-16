dd921718ded723d67c9c9856d27e77dc59ea1648
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-08-16 09:24:36 -0400

    update

--- src/py/filter/dictionary_of_dictionaries/filterByDate.py
+++ src/py/filter/dictionary_of_dictionaries/filterByDate.py
@@ -1,5 +1,5 @@
-# filterBetweenDates.py
-# returns only the items from a dictionary that have a date between two given cutoff dates
+# filterByDate.py
+# returns only the items from a dictionary that match the given calendar date (ignores the time)
 
 from datetime import datetime
 
@@ -15,50 +15,49 @@ people = {
     "tabitha": {
         "name": "Tabitha",
         "date": "2024-05-09 08:45:00"
+    },
+    "ariana": {
+        "name": "Ariana",
+        "date": "2024-05-01 14:20:00"
     }
 }
 
-def filterBetweenDates(whichList, startDate, endDate):
+def filterByDate(whichList, targetDate):
     # start with an empty dictionary
     result = {}
 
-    # convert the startDate and endDate strings into datetime objects
-    start = datetime.strptime(startDate, "%Y-%m-%d %H:%M:%S")
-    end = datetime.strptime(endDate, "%Y-%m-%d %H:%M:%S")
+    # convert the targetDate string into a datetime object (date only)
+    target = datetime.strptime(targetDate, "%Y-%m-%d").date()
 
     # loop through every key in the given dictionary
     for key in whichList:
         # grab the date string from the current item
         dateString = whichList[key]['date']
 
-        # convert the date string into a datetime object
-        itemDate = datetime.strptime(dateString, "%Y-%m-%d %H:%M:%S")
+        # convert the date string into a datetime object, then just take the date part
+        itemDate = datetime.strptime(dateString, "%Y-%m-%d %H:%M:%S").date()
 
-        # if the item date is between the two cutoffs, keep it
-        if (itemDate >= start and itemDate <= end):
+        # if the item date matches the target date, keep it
+        if (itemDate == target):
             # assign the entire profile object to our new dictionary using the same key
             result[key] = whichList[key]
 
-    # return the new dictionary containing only items in the given range
+    # return the new dictionary containing only items with the given date
     return result
 
 if __name__ == '__main__':
-    # call the function with our people dictionary, searching between May 1 and May 8, 2024
-    betweenDatePeople = filterBetweenDates(
-        people,
-        "2024-05-01 00:00:00",
-        "2024-05-08 23:59:59"
-    )
+    # call the function with our people dictionary, searching for May 1, 2024 (ignores the time)
+    datePeople = filterByDate(people, "2024-05-01")
 
     # print the filtered dictionary so we can see the result
-    print(betweenDatePeople)
+    print(datePeople)
 
     input('Press Enter to Exit')
 
 ####
 
 '''
-{'jane': {'name': 'Jane', 'date': '2024-05-01 10:00:00'}}
+{'jane': {'name': 'Jane', 'date': '2024-05-01 10:00:00'}, 'ariana': {'name': 'Ariana', 'date': '2024-05-01 14:20:00'}}
 '''
 
 '''
@@ -67,11 +66,14 @@ if __name__ == '__main__':
     {
         'name': 'Jane',
         'date': '2024-05-01 10:00:00'
+    },
+    'ariana':
+    {
+        'name': 'Ariana',
+        'date': '2024-05-01 14:20:00'
     }
 }
 '''
-
-####
 
 # Dedicated to God the Father
 # All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
