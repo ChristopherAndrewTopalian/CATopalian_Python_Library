e4f0134d8406dc9134df92ba51f7791ad46f7206
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-08-16 09:54:20 -0400

    update

---
+++ src/py/filter/dictionary_of_dictionaries/filterOutsideDates.py
@@ -0,0 +1,88 @@
+# filterOutsideDates.py
+# returns only the items from a dictionary that have a date outside of two given cutoff dates
+
+from datetime import datetime
+
+people = {
+    "jane": {
+        "name": "Jane",
+        "date": "2024-05-01 10:00:00"
+    },
+    "jennifer": {
+        "name": "Jennifer",
+        "date": "2023-03-12 15:30:00"
+    },
+    "tabitha": {
+        "name": "Tabitha",
+        "date": "2024-05-09 08:45:00"
+    }
+}
+
+####
+
+def filterOutsideDates(whichList, startDate, endDate):
+    # start with an empty dictionary
+    result = {}
+
+    # convert the startDate and endDate strings into datetime objects
+    start = datetime.strptime(startDate, "%Y-%m-%d %H:%M:%S")
+    end = datetime.strptime(endDate, "%Y-%m-%d %H:%M:%S")
+
+    # loop through every key in the given dictionary
+    for key in whichList:
+        # grab the date string from the current item
+        dateString = whichList[key]['date']
+
+        # convert the date string into a datetime object
+        itemDate = datetime.strptime(dateString, "%Y-%m-%d %H:%M:%S")
+
+        # if the item date is before the start OR after the end, keep it
+        if (itemDate < start or itemDate > end):
+            # assign the entire profile object to our new dictionary using the same key
+            result[key] = whichList[key]
+
+    # return the new dictionary containing only items outside the range
+    return result
+
+####
+
+if __name__ == '__main__':
+    # call the function with our people dictionary, using May 1 to May 8, 2024 as the range
+    outsideDatePeople = filterOutsideDates(
+        people,
+        "2024-05-01 00:00:00",
+        "2024-05-08 23:59:59"
+    )
+
+    # print the filtered dictionary so we can see the result
+    print(outsideDatePeople)
+
+    input('Press Enter to Exit')
+
+####
+
+'''
+{'jennifer': {'name': 'Jennifer', 'date': '2023-03-12 15:30:00'}, 'tabitha': {'name': 'Tabitha', 'date': '2024-05-09 08:45:00'}}
+'''
+
+'''
+{
+    'jennifer':
+    {
+        'name': 'Jennifer',
+        'date': '2023-03-12 15:30:00'
+    },
+    'tabitha':
+    {
+        'name': 'Tabitha',
+        'date': '2024-05-09 08:45:00'
+    }
+}
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
