5361695e036fe50428af3d8fd9c76e1d7fc02428
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-08-20 17:03:57 -0400

    update

--- src/py/date_time/get_date_time_12.py
+++ src/py/date_time/get_date_time_12.py
@@ -20,21 +20,16 @@ def get_date_time_12():
 
 if __name__ == '__main__':
     print(get_date_time_12())
-
     input('Press Enter to Exit')
-
-####
 
 '''
 09-06-25 05:10 AM
 Press Enter to Exit
 '''
 
-####
-
 # Dedicated to God the Father
-# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
-# https://github.com/ChristopherTopalian
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
 # https://github.com/ChristopherAndrewTopalian
+# https://github.com/ChristopherTopalian
 # https://sites.google.com/view/CollegeOfScripting
 
